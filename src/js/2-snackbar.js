1d5b2438aad5cb4084bd946fee66d8076938b3a9
daria drozd <daria.drozd1990@gmail.com>
2024-05-11 18:26:38 +0300

    snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,16 +1,52 @@
 // Описаний у документації
-import iziToast from "izitoast";
+import iziToast from 'izitoast';
 // Додатковий імпорт стилів
-import "izitoast/dist/css/iziToast.min.css";
+import 'izitoast/dist/css/iziToast.min.css';
 
-iziToast.show({
-  
-    message: `✅ Fulfilled promise in ${delay}ms`
-    
-});
-iziToast.show({
-  
-    message: `❌ Rejected promise in ${delay}ms`
+const refs = {
+  form: document.querySelector('.form'),
+  input: document.querySelector('.js-input'),
+  stateRadios: document.querySelectorAll('.js-state-radio'),
+  submitBTN: document.querySelector('.js-submit'),
+};
 
-    
-});
+refs.form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event){
+    event.preventDefault();
+
+    const delay = parseInt(refs.input.value);
+    const state = getState(); 
+
+
+    const promise =new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+        
+    });
+     promise
+    .then(delay => {
+        // Успішне виконання промісу
+        iziToast.show({
+            message: `✅ Fulfilled promise in ${delay}ms`,
+        });
+    })
+    .catch(delay => {
+        // Відхилення промісу
+        iziToast.show({
+            message: `❌ Rejected promise in ${delay}ms`,
+        });
+    });
+}
+function getState() {
+    for (const radio of refs.stateRadios) {
+        if (radio.checked) {
+            return radio.value;
+        }
+    }
+}
