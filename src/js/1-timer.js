be6fab1bbd93361026e1d20ce92abe38863b9f05
daria drozd <daria.drozd1990@gmail.com>
2024-05-12 21:04:23 +0300

    timer рефакторинг

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -68,14 +68,7 @@ function startTimer() {
       return;
     }
 
-    const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor(
-      (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
-    );
-    const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+    const { days, hours, minutes, seconds } = convertMs(remainingTime);
 
     // Форматуємо та відображаємо час у форматі xx:xx:xx:xx
     refs.timerDisplay.textContent = `${formatTime(days)}:${formatTime(
@@ -86,3 +79,25 @@ function startTimer() {
 function formatTime(time) {
   return time < 10 ? `0${time}` : time;
 }
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
